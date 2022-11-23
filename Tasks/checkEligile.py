5035d41bf79da680de33499527f6379ba91f148d
qapbash <bari.qapbash@gmail.com>
2022-11-23 07:47:52 +0100

    oddNums

---
+++ Tasks/checkEligile.py
@@ -0,0 +1,9 @@
+def check_eligible():
+    age = int(input("Enter your age"))
+    if age >=18:
+        print("Yes")
+    else:
+        print("No")
+
+
+check_eligible()
---
+++ changeLastElement.py
@@ -0,0 +1,5 @@
+
+list = ["Bari",17,"Filora",119,"Waris",720,23]
+list[len(list)-1]=0
+print(list)
+
